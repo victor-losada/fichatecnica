c6a60441cae6432ad1612a071e8801b72243d63f
vanec06 <vanesacha06@gmail.com>
2023-04-10 16:42:15 -0500

    hola

---
+++ js/formato.js
@@ -0,0 +1,9 @@
+function comprobar(){
+    
+    let nombre
+    nombre = document.getElementById("nombre").value
+
+
+    
+    document.getElementById("respuesta").innerHTML="<h1> nombre del producto: " + nombre + "</h1>"
+}
